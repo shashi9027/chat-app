cd5ec5ec65ee41270f87e075e9862ee4008418d5
unknown <shashi902794@gmail.com>
2024-03-02 12:47:50 +0530

    login form with wrapper

--- components/cards/LoginForm.tsx
+++ components/cards/LoginForm.tsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import { FormProvider } from "react-hook-form";
+import * as Yup from "yup"
+import {useForm} from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+
 export default function LoginForm(){
+    const [showPassword, setShowPassword] = useState(false);
+    const LoginSchema = Yup.object().shape({
+        email: Yup.string().required("Email is required").email("Email must be a valid email address"),
+        password: Yup.string().required("Password is required")
+    })
+    const defaultValues ={
+        email: "demo@tawk.com",
+        password: "demo1234"
+    }
+    const methods = useForm({
+        resolver: yupResolver(LoginForm),
+        defaultValues
+    })
+
+    const {reset, setError , handleSubmit, formState:{errors, isSubmitting, isSubmitSuccessful}} = methods;
+    
+    const onSubmit = async (data:any) =>{
+       try{
+
+       }
+       catch(error:any){
+       
+        reset()
+        setError("afterSubmit", {
+            ...error,
+            message: errors.message
+            
+        })
+       }
+    }
+
     return(
-        <div>Login Form</div>
+       <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        
+       </FormProvider>
     )
 }
--- components/hook-form/FormProvider.tsx
+++ components/hook-form/FormProvider.tsx
@@ -1,5 +1,19 @@
-const FormProvider = () =>{
+import React, {ReactNode} from "react"
+import {FormProvider as Form} from "react-hook-form"
+
+
+interface FormProviderProps {
+    children: ReactNode;
+    onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+    methods: any; 
+  }
+
+const FormProvider = ({children, onSubmit, methods}: FormProviderProps) =>{
     return(
-        <Form></Form>
+        <Form {...methods}>
+          <form onSubmit={onSubmit}>
+           {children}
+           </form>
+        </Form>
     )
 }
